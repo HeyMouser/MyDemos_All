81b05d6e627bbae3201018abf1cf27cbf0d3aef3
HeyYou <y872701723@163.com>
2018-10-21 10:44:58 +0800

    create res

---
+++ AutoControlWeChat/app/src/main/java/com/yh/autocontrolwechat/MainActivity.java
@@ -0,0 +1,13 @@
+package com.yh.autocontrolwechat;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
